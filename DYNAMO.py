851789aa559aabd3fc42e612f4328f46ec1c6ee2
Anunay-Alok <44166023+Anunay-Alok@users.noreply.github.com>
2020-06-03 14:00:00 +0530

    Create DYNAMO.py

---
+++ DYNAMO.py
@@ -0,0 +1,10 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = int(input())
+    print(2*(10**n)+a)
+    a=int(input());
+    print((10**n)-a)
+    a=int(input())
+    print((10**n)-a)
+    if int(input())==-1:
+        break
